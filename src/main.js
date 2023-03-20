a0a593a562bb56945c383f48b23421e1b0fcd675
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-03-21 00:58:13 +0300

    Added home page component, imported the fake json data, and also added a router to the hompage

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHistory} from "vue-router";
+import PageHome from "@/components/PageHome.vue";
 
-createApp(App).mount('#app')
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: PageHome
+    },
+]
+
+const router = createRouter({
+
+    history: createWebHistory(),
+    routes,
+})
+
+const forumApp = createApp(App)
+forumApp.use(router)
+forumApp.mount('#app')
+
+// createApp(App).mount('#app')
