405eb96ddb5002509f563436614b9df380abb80c
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-03-30 16:33:51 +0300

    Started implementing the not found redirection for the not found threads parameters

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory} from "vue-router";
 import PageHome from "@/components/PageHome.vue";
 import PageThreadShow from "@/components/PageThreadShow.vue";
 import PageNotFound from "@/components/PageNotFound.vue";
+import sourceData from '@/data.json'
 
 const routes = [
     {
@@ -13,7 +14,18 @@ const routes = [
         path: '/thread/:id',
         name: 'ThreadShow',
         component: PageThreadShow,
-        props: true
+        props: true,
+        beforeEnter(to, from, next){
+        //     check of the thread with specified id exists
+            const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+        //     If exists, Continue
+            if (threadExists){
+                return next()
+            } else {
+                next({name: 'NotFound'})
+            }
+        //     if doesn't exist, redirect the user to not found page
+        }
     },
     {
         path: '/:pathMatch(.*)',
