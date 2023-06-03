3a668c1388c130343a797a3e4a02a7fdc17183e1
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-06-03 13:24:48 +0300

    Added human readable dates with a tooltip to show localized time

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="post-list">
-    <div class="post"
-         v-for="post in posts"
-         :key="post.id"
-    >
+    <div class="post" v-for="post in posts" :key="post.id">
       <div class="user-info">
         <a href="#" class="user-name">{{ userById(post.userId).name }}</a>
         <a href="#">
-          <img :src="userById(post.userId).avatar" alt="" class="avatar-large">
+          <img
+            :src="userById(post.userId).avatar"
+            alt=""
+            class="avatar-large"
+          />
         </a>
-        <p class="desktop-only text-small"> 107 posts</p>
+        <p class="desktop-only text-small">107 posts</p>
       </div>
 
       <div class="post-content">
@@ -20,37 +21,51 @@
         </div>
       </div>
 
-      <div class="post-date text-faded">
-        {{post.publishedAt}}
+      <div
+        class="post-date text-faded"
+        :title="humanFriendlyDate(post.publishedAt)"
+      >
+        {{ diffForHumans(post.publishedAt) }}
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import sourceData from "@/data.json"
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import localizedDate from "dayjs/plugin/localizedFormat";
+dayjs.extend(localizedDate);
+dayjs.extend(relativeTime);
+import sourceData from "@/data.json";
 export default {
   name: "PostList",
-data() {
-  return {
-    users: sourceData.users
-  }
-},
+  data() {
+    return {
+      users: sourceData.users,
+    };
+  },
   // declare the props
   props: {
     posts: {
       type: Array,
-      required: true
-    }
+      required: true,
+    },
   },
   methods: {
     userById(userId) {
-      return this.users.find(p => p.id === userId)
-    }
-  }
-}
+      return this.users.find((p) => p.id === userId);
+    },
+    diffForHumans(timestamp) {
+      return dayjs(timestamp * 1000).fromNow();
+    },
+    humanFriendlyDate(timestamp) {
+      // return dayjs(timestamp * 1000).format("MMMM DD, YYYY HH:mm");
+      return dayjs.unix(timestamp * 1000).format("LLL");
+    },
+  },
+  // create a dayjs methode to show time elapsed since post
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
